66c0ee02b2a1ae0461e4d566421cfad944743a02
underengineering <san4a852b@gmail.com>
2023-08-10 23:28:29 +0300

    Add more methods to sysinfo api

--- src/luaapi/sysinfo.rs
+++ src/luaapi/sysinfo.rs
@@ -1,5 +1,11 @@
+use std::collections::HashMap;
+
 use mlua::prelude::*;
-use sysinfo::{CpuRefreshKind, Pid, ProcessRefreshKind, System, SystemExt};
+use serde::{Deserialize, Serialize};
+use sysinfo::{
+    Cpu, CpuExt, CpuRefreshKind, NetworkExt, NetworksExt, Pid, ProcessRefreshKind, System,
+    SystemExt,
+};
 
 use crate::system_info::battery;
 
@@ -54,6 +60,7 @@ fn add_system_api(lua: &Lua, sysinfo_table: &LuaTable) -> LuaResult<()> {
     sysinfo_table.set("ProcessRefreshKind", process_refresh_kind)?;
 
     lua.register_userdata_type::<System>(|reg| {
+        // Refresh methods
         reg.add_method_mut("refresh_all", |_, this, ()| {
             this.refresh_all();
             Ok(())
@@ -144,6 +151,94 @@ fn add_system_api(lua: &Lua, sysinfo_table: &LuaTable) -> LuaResult<()> {
         reg.add_method_mut("refresh_components_list", |_, this, ()| {
             this.refresh_components_list();
             Ok(())
+        });
+
+        // Getters
+        fn push_cpu(cpu: &Cpu, table: &LuaTable) -> LuaResult<()> {
+            table.set("name", cpu.name())?;
+            table.set("frequency", cpu.frequency())?;
+            table.set("vendor_id", cpu.vendor_id())?;
+            table.set("brand", cpu.brand())?;
+            table.set("cpu_usage", cpu.cpu_usage())?;
+            Ok(())
+        }
+
+        reg.add_method("cpus", |lua, this, ()| {
+            let cpus = this.cpus();
+            let cpus_table = lua.create_table()?;
+            for (i, cpu) in cpus.iter().enumerate() {
+                let cpu_table = lua.create_table()?;
+                push_cpu(cpu, &cpu_table)?;
+                cpus_table.set(i, cpu_table)?;
+            }
+
+            Ok(cpus_table)
+        });
+
+        reg.add_method("global_cpu_info", |lua, this, ()| {
+            let cpu = this.global_cpu_info();
+            let cpu_table = lua.create_table()?;
+            push_cpu(cpu, &cpu_table)?;
+
+            Ok(cpu_table)
+        });
+
+        reg.add_method("total_memory", |_, this, ()| {
+            Ok(LuaValue::Integer(this.total_memory() as i64))
+        });
+
+        reg.add_method("available_memory", |_, this, ()| {
+            Ok(LuaValue::Integer(this.available_memory() as i64))
+        });
+
+        reg.add_method("used_memory", |_, this, ()| {
+            Ok(LuaValue::Integer(this.used_memory() as i64))
+        });
+
+        reg.add_method("free_memory", |_, this, ()| {
+            Ok(LuaValue::Integer(this.free_memory() as i64))
+        });
+
+        reg.add_method("total_swap", |_, this, ()| {
+            Ok(LuaValue::Integer(this.total_swap() as i64))
+        });
+
+        reg.add_method("free_swap", |_, this, ()| {
+            Ok(LuaValue::Integer(this.free_swap() as i64))
+        });
+
+        reg.add_method("used_swap", |_, this, ()| {
+            Ok(LuaValue::Integer(this.used_swap() as i64))
+        });
+
+        reg.add_method("networks", |lua, this, ()| {
+            let networks_table = lua.create_table()?;
+            for (iface, data) in this.networks() {
+                let data_table = lua.create_table()?;
+                macro_rules! copy_fields {
+                    ($name:ident) => {
+                        data_table.set(stringify!($name), data.$name())?;
+                    };
+                }
+
+                copy_fields!(received);
+                copy_fields!(total_received);
+                copy_fields!(transmitted);
+                copy_fields!(total_transmitted);
+                copy_fields!(packets_received);
+                copy_fields!(total_packets_received);
+                copy_fields!(packets_transmitted);
+                copy_fields!(total_packets_transmitted);
+                copy_fields!(errors_on_received);
+                copy_fields!(total_errors_on_received);
+                copy_fields!(errors_on_transmitted);
+                copy_fields!(total_errors_on_transmitted);
+                data_table.set("mac_address", data.mac_address().0)?;
+
+                networks_table.set(iface.as_str(), data_table)?;
+            }
+
+            Ok(networks_table)
         });
     })?;
     let system = lua.create_table()?;
