17af527586aaefa99eafd1facaf97f41be66c4f2
underengineering <san4a852b@gmail.com>
2023-08-11 21:14:59 +0300

    Fix scss functions not returning data

--- src/luaapi/utils.rs
+++ src/luaapi/utils.rs
@@ -5,16 +5,14 @@ fn add_grass_api(lua: &Lua, utils_table: &LuaTable) -> LuaResult<()> {
     utils_table.set(
         "scss_from_path",
         lua.create_function(|_, path: String| {
-            grass::from_path(path, &grass::Options::default()).unwrap();
-            Ok(())
+            Ok(grass::from_path(path, &grass::Options::default()).unwrap())
         })?,
     )?;
 
     utils_table.set(
         "scss_from_string",
         lua.create_function(|_, input: String| {
-            grass::from_string(input, &grass::Options::default()).unwrap();
-            Ok(())
+            Ok(grass::from_string(input, &grass::Options::default()).unwrap())
         })?,
     )?;
 
