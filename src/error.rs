f8c957a48c8f8b4bb2d73d9e2faaff780e135dd2
underengineering <san4a852b@gmail.com>
2023-08-19 19:21:22 +0300

    Fix clippy warnings

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,9 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Lua error")]
-    LuaError(#[from] LuaError),
+    Lua(#[from] LuaError),
     #[error("I/O error")]
-    IoError(#[from] io::Error),
+    Io(#[from] io::Error),
     #[error("Config file does not exist")]
     ConfigFileDoesNotExist,
     #[error("Config file is not a file")]
