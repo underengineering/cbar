e202d43d79e58125d4d25d57227ec06a38cf95bf
underengineering <san4a852b@gmail.com>
2023-08-24 23:17:24 +0300

    hyprland: Remove unnecessary conversion

--- src/hyprland/ipc/mod.rs
+++ src/hyprland/ipc/mod.rs
@@ -25,16 +25,13 @@ async fn try_request<T: Command>(buffer: &mut Vec<u8>) -> Result<(), Error> {
     let conn = sock
         .connect_future(&UnixSocketAddress::new(socket_path))
         .await?;
-    let stream = conn.into_async_read_write().unwrap();
+    let mut stream = conn.into_async_read_write().unwrap();
 
-    let mut writer = stream.output_stream().clone().into_async_write().unwrap();
-    let mut reader = stream.input_stream().clone().into_async_read().unwrap();
-
-    writer
+    stream
         .write_all(format!("j/{}", T::NAME).as_bytes())
         .await?;
 
-    reader.read_to_end(buffer).await?;
+    stream.read_to_end(buffer).await?;
 
     Ok(())
 }
